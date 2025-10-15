249b30cb437fedfca33f670367cbddfad3d41b56
kk4569a <kk4569a@gmail.com>
2025-10-16 01:47:02 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // 読み込み時の処理
 window.addEventListener('load', function () {
     document.getElementsByTagName('html')[0].classList.add('no-scroll')
+    document.getElementsByTagName('body')[0].classList.add('no-scroll')
     document.getElementsByClassName('loading-window')[0].classList.add('active')
     const coverWindow = document.getElementsByClassName('cover-window')
     coverWindow[0].classList.add('active')
@@ -24,6 +25,7 @@ window.addEventListener('load', function () {
     
     setTimeout(() => {
         document.getElementsByTagName('html')[0].classList.remove('no-scroll')
+        document.getElementsByTagName('body')[0].classList.remove('no-scroll')
         document.getElementsByTagName('html')[0].removeAttribute('onwheel')  //慣性スクロールオン
         for(let i = 0; i < document.getElementsByClassName('loaded-anime').length; i++) {
             document.getElementsByClassName('loaded-anime')[i].classList.remove('loaded')
@@ -151,6 +153,7 @@ function humburgerMenu() {
         humburgerMenuFlag = true;
         document.getElementsByTagName('html')[0].setAttribute('onwheel', 'event.stopPropagation()')  //慣性スクロールオフ
         document.getElementsByTagName('html')[0].classList.add('no-scroll')
+        document.getElementsByTagName('body')[0].classList.add('no-scroll')
         document.getElementsByClassName('cover-window')[0].classList.add('nav-open')
         document.getElementsByClassName('cover-window')[0].style.left = "-44%"
     }
@@ -164,6 +167,7 @@ function humburgerMenu() {
         setTimeout(() => {    
             humburgerMenuFlag = false;
             document.getElementsByTagName('html')[0].classList.remove('no-scroll')
+            document.getElementsByTagName('body')[0].classList.remove('no-scroll')
             document.getElementsByTagName('html')[0].removeAttribute('onwheel')  //慣性スクロールオン
             document.getElementsByClassName('cover-window')[0].classList.remove('nav-open')
         }, 400);
